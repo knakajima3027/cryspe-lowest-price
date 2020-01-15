b3474814aa5794acd13c1c4e43607584acd6c2cc
kNakajima3027 <kamihate1012@gmail.com>
2020-01-15 17:04:27 +0900

    first commit

---
+++ server/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+# config.pyを参照して設定を行う
+app.config.from_object('app.config')
+
+# データベースの定義
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
---
+++ server/run.py
@@ -0,0 +1,2 @@
+from app import app
+app.run(host='localhost', port=3000)
