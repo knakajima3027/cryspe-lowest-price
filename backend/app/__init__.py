d511a3b25047a0e077b8f1704547d4a1dbf97e3c
knakajima3027 <kamihate1012@gmail.com>
2020-01-16 17:37:38 +0900

    sample API

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from app.models import Card
 
 app = Flask(__name__)
 
+# カード情報を全て返すAPI
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cards = Card.query.all()
+
+    result = []
+    for card in cards:
+        result_json = {
+            'name': card.card_name,
+            'price': card.card_price,
+            'img': card.img_url,
+            'url': card.url
+        }
+        result.append(result_json)
+
+    return jsonify(result) 
